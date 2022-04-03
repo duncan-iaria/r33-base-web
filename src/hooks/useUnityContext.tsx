a14542aefc7216aa4912f980724052974ca6ca0c
duncan-iaria <duncaniaria@gmail.com>
2022-04-02 22:51:45 -0400

    🔧 changed builds to using ones hosted on the digital ocean CDN

--- src/hooks/useUnityContext.tsx
+++ src/hooks/useUnityContext.tsx
@@ -3,11 +3,21 @@ import { UnityContext } from 'react-unity-webgl';
 
 export const GAME_NAME = 'ForgeBots Riptide';
 
+const locaLoaderSrc =  './Build/Riptide_01.loader.js'
+const localDataUrlSrc = './Build/Riptide_01.data';
+const localFrameworkUrlSrc = './Build/Riptide_01.framework.js';
+const localWasmCodeSrc = './Build/Riptide_01.wasm';
+
+const loaderSrc = "https://forgebots.nyc3.cdn.digitaloceanspaces.com/builds/gem-collector/gem-collector.loader.js"
+const dataUrlSrc = "https://forgebots.nyc3.cdn.digitaloceanspaces.com/builds/gem-collector/gem-collector.data";
+const frameworkUrlSrc = "https://forgebots.nyc3.cdn.digitaloceanspaces.com/builds/gem-collector/gem-collector.framework.js"
+const wasmCodeSrc = "https://forgebots.nyc3.cdn.digitaloceanspaces.com/builds/gem-collector/gem-collector.wasm";
+
 const unityContext = new UnityContext({
-  loaderUrl: './Build/Riptide_01.loader.js',
-  dataUrl: './Build/Riptide_01.data',
-  frameworkUrl: './Build/Riptide_01.framework.js',
-  codeUrl: './Build/Riptide_01.wasm',
+  loaderUrl: loaderSrc,
+  dataUrl: dataUrlSrc,
+  frameworkUrl: frameworkUrlSrc,
+  codeUrl: wasmCodeSrc
 });
 
 export const UnityContextData = createContext(unityContext);
